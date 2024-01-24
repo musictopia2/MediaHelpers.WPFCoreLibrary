9e25cd7499fbd25c786c61869de60d31aa970dbe
Andy Walter <musictopia2@hotmail.com>
2024-01-23 18:48:52 -0700

    Added code to load the video after clicking start on remote control.

--- Windows/WelcomeComponent.razor.cs
+++ Windows/WelcomeComponent.razor.cs
@@ -14,10 +14,19 @@ public partial class WelcomeComponent<E>
     protected override void OnInitialized()
     {
         _rets = Begins!.CanPlay;
+
         if (_rets)
         {
+            LoadEpisode();
             //if you are able to play, then needs to run the video loader to load up.
-            Loader!.ChoseEpisode(Container!.EpisodeChosen!);
         }
+        else
+        {
+            Begins.StartLoadingPlayer = LoadEpisode; //blazor won't change because its going to xaml screen with little blazor.
+        }
+    }
+    private void LoadEpisode()
+    {
+        Loader!.ChoseEpisode(Container!.EpisodeChosen!);
     }
 }
